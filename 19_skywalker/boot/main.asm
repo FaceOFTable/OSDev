495582d0597178079756b0a31fe6843bebc60c82
HFoxtail <hplus.foxtail@gmail.com>
2018-08-11 20:37:24 +0400

    update boot

--- 19_skywalker/boot/main.asm
+++ 19_skywalker/boot/main.asm
@@ -1,11 +1,9 @@
 
         org     $7C00
         macro   brk { xchg bx, bx }
-        
-DRIVE   equ $0000        
 
         ; 1. Загрузка BIOS $FFFF : 0000
-        ; 2. Загрузка 512 байт с MBR (0-й сектор) 
+        ; 2. Загрузка 512 байт с MBR (0-й сектор)
         ; 3. Загрузчик Bootloader загружает в память стартер (kernel)
         ; 4. Стартер запускает ядро и загружает в память ОС
 
@@ -21,10 +19,10 @@ DRIVE   equ $0000
         mov     ds, ax
         mov     es, ax
         mov     [$0000], dl     ; В регистре dl будет номер диска BIOS
-    
+
 ; Поиск первого раздела с FAT32
 ; ----------------------------------------------------------------------
-        
+
         mov     cx, 4
         mov     si, 1BEh + 7C00h
 
@@ -34,8 +32,8 @@ retry_find:
         je      partition_found
         add     si, 16
         loop    retry_find
-                
-        mov     si, msg_notfound_partition
+
+        mov     si, msg_notfound
         jmp     show_error
 
 ; Определение структуры FAT
@@ -45,12 +43,12 @@ partition_found:
 
         mov     ebp, [si + 8]           ; Считывается первый сектор
         mov     [DAP_sector + 8], ebp   ; Номер сектора, где хранится 1-й сектор
-        
+
         ; Скачать сектор
         mov     ah, 42h
         mov     si, DAP_sector
-        int     13h    
-                
+        int     13h
+
         ; 1 BPB
         ; n Резервированные сектора
         ; m x fat_count Сектора FAT
@@ -61,7 +59,7 @@ partition_found:
         ; Вычислить начало FAT
         movzx   ebx, word [si + $0E]     ; Зарезервированные сектора
         add     ebp, ebx
-        mov     [FAT_sector], ebp        ; ebp = адрес сектора с FAT   
+        mov     [FAT_sector], ebp        ; ebp = адрес сектора с FAT
 
         ; Вычисление сектора данных
         mov     eax, [si + $24]          ; Секторов в FAT (32)
@@ -73,7 +71,7 @@ partition_found:
         ; Секторов в кластере
         mov     al,  [si + $0D]
         mov     [DAP_cluster + 2], al
-        
+
         ; Первый кластер корневого каталога
         mov     eax, [si + $2C]
 
@@ -83,7 +81,7 @@ partition_found:
 search_loop:
 
         call    load_cluster
-        
+
         mov     di, $8000
         mov     dx, [DAP_cluster + 2]   ; секторов на кластер
         shl     dx, 4                   ; 1 сектор = 16 записей
@@ -97,45 +95,44 @@ row_loop:
         repz    cmpsb
         and     cx, cx
         je      file_found
-        
+
         ; К следующей записи
-        mov     di, bx
-        add     di, 32
+        lea     di, [bx + 32]
         dec     dx
         jne     row_loop
-        
+
         ; Поиск следующего кластера в FAT
         call    next_cluster
-        
+
         ; Проверка на конец последовательности
         cmp     eax, $0FFFFFF8
         jc      search_loop
 
         ; Файл не был найден
-        mov     si, msg_notfound_partition
+        mov     si, msg_notfound
         jmp     show_error
 
 ; Загрузка файла в память
 ; ----------------------------------------------------------------------
 
 file_found:
-        
+
         ; Получается адрес кластера
         mov     ax, [bx + $14]          ; старшие 2 байта кластера
         shl     eax, 16
         mov     ax, [bx + $1A]          ; нижние 2 байта кластера
-        
+
 load_file:
-        
+
         call    load_cluster
         call    next_cluster
-        mov     cx, [DAP_cluster + 2]    ; 1 s = 512/16 = 32        
+        mov     cx, [DAP_cluster + 2]    ; 1 s = 512/16 = 32
         shl     cx, 5                    ; Умножение числа секторов на 32 сегмента
         add     [DAP_cluster + 6], cx    ; Сместим область загрузки на 512 * Sectors_Per_Cluster
         cmp     eax, $0FFFFFF8
         jc      load_file
 
-; Выполнение кода        
+; Выполнение кода
 ; ----------------------------------------------------------------------
 
         brk
@@ -156,37 +153,37 @@ load_cluster:
         add     eax, ebp
         mov     [DAP_cluster + 8], eax
         mov     si, DAP_cluster
-        call    load_data        
+        call    load_data
         popa
         ret
-        
+
 ; ----------------------------------------------------------------------
 ; I: EAX - текущий кластер
 ; O: EAX - новый кластер
 
 next_cluster:
-        
+
         ; 512 / 4 = 128 записей в 1 секторе
         ; Расчет сектора, где будет следующий кластер
         mov     ebx, eax
-        shr     ebx, 7      ; num / 128 
+        shr     ebx, 7      ; num / 128
         add     ebx, [FAT_sector]
         mov     [DAP_sector + 8], ebx
-        
+
         mov     si, DAP_sector
         call    load_data       ; -> 7E00h
-        
+
         mov     bx, ax
         and     bx, 7Fh         ; eax = eax % 128
         shl     bx, 2           ; eax = eax * 4
-        
+
         mov     eax, [$7E00 + bx] ; Получен новый номер кластера
-        ret            
+        ret
 
 ; ----------------------------------------------------------------------
 ; SI - указатель на структуру
 
-load_data:   
+load_data:
 
         push    ax
         mov     dl, [$0000]         ; Номер диска
@@ -194,7 +191,6 @@ load_data:
         int     13h
         pop     ax
         ret
-
 
 ; ----------------------------------------------------------------------
 show_error:
@@ -206,16 +202,15 @@ show_error:
         int     10h
         jmp     show_error
 
-msg_notfound_partition db "Data not found", 0
+msg_notfound db "Data not found", 0
 
 ; ----------------------------------------------------------------------
 
+Find_File    db 'CORE    BIN'
 FAT_sector   dd 0
 FAT_data     dd 0
 
-Find_File    db 'CORE    BIN'
-
-DAP_sector: 
+DAP_sector:
 
         ; 7C00 - 7DFF 1-й MBR
         ; 7E00 - 7FFF 1-й FAT32
