f956002b13b716d03120c24dcd8be5dc956bbff0
HFoxtail <hplus.foxtail@gmail.com>
2018-12-16 13:29:15 +0400

    improved

--- 20_another/kernel.c
+++ 20_another/kernel.c
@@ -9,8 +9,16 @@ void main() {
     irq_init(IRQ_KEYB);
     
     vga_mode(VGA_640x480);
-    vga_cls(1);    
-    vga_putf8(0,0,"Привет, UTF8",15);
+    vga_cls(3);    
+    
+    
+    //vga_block(6, 15, 17, 161, 15);
+    //for(;;);
+    
+    vga_block(15, 15, 481, 161, 0);
+    vga_block(16, 16, 480, 160, 7);
+    vga_block(17, 17, 479, 17+18, 1);
+    vga_putf8(24, 19, "Вендеканец 95", 15);
     
     sti;
 
--- 20_another/kernel/vga.c
+++ 20_another/kernel/vga.c
@@ -119,6 +119,7 @@ void vga_pixel(unsigned x, unsigned y, unsigned char c) {
 
         // Читать перед записью, иначе не сработает
         volatile uint8_t t = vaddr[ symbol ];
+
         vaddr[ symbol ] = c;
     }
 }
@@ -215,9 +216,9 @@ void vga_putf8(int x, int y, char* string, char color) {
         }
 
         // Псевдографика недоступна
-        vga_pchar(8*x, 16*y, chr, color);
+        vga_pchar(x, y, chr, color);
 
-        x += 1;
+        x += 8;
         string++;
     }
 
@@ -242,15 +243,50 @@ void vga_dotted_block(int x1, int y1, int x2, int y2, uint8_t color) {
     }
 }
 
-// Блок сплошного цвета
+// Блок сплошного цвета @todo оптимизировать его
 void vga_block(int x1, int y1, int x2, int y2, uint8_t color) {
 
-    int i, j;
-    for (i = y1; i <= y2; i++) {
-        for (j = x1; j <= x2; j++) {
-            vga_pixel(j, i, color);
+    int i, j, x;
+
+    int x1i = x1 >> 3;
+    int x2i = x2 >> 3;
+
+    unsigned int xl = (  0xFF00 >> (x1 & 7)) & 0xFF00;
+    unsigned int xr = (0x7F8000 >> (x2 & 7)) & 0xFF00;
+
+    // Общая быстрая маска
+    IoWrite16(VGA_GC_INDEX, 0xFF08);
+
+    if (x1i + 1 < x2i) {
+
+        for (i = y1; i <= y2; i++) {
+
+            char* vm = (char*)(0xA0000 + 80*i);
+            for (j = x1i + 1; j < x2i; j++) {
+                volatile char t = vm[j]; vm[j] = color;
+            }
         }
     }
+
+    // Одновременная
+    if (x1i == x2i) {
+
+        IoWrite16(VGA_GC_INDEX, (xl & xr) | 0x08);
+        for (i = y1; i <= y2; i++) { char* vm = (char*)(0xA0000 + 80*i); volatile char t = vm[x1i]; vm[x1i] = color; }
+    }
+    
+    // Левая и правая часть
+    else {
+
+        // Слева
+        IoWrite16(VGA_GC_INDEX, xl | 0x08);
+        for (i = y1; i <= y2; i++) { char* vm = (char*)(0xA0000 + 80*i); volatile char t = vm[x1i]; vm[x1i] = color; }
+
+        // Справа
+        IoWrite16(VGA_GC_INDEX, xr | 0x08);
+        for (i = y1; i <= y2; i++) { char* vm = (char*)(0xA0000 + 80*i); volatile char t = vm[x2i]; vm[x2i] = color; }
+    }
+
 }
 
 /*
